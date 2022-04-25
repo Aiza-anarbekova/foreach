69828cf9ab0877c58d92a7c49bc24ab73448450b
HP <aiza0000123@gmail.com>
2022-04-25 16:22:54 +0300

    version2

--- src/com/compan/foreach2.java
+++ src/com/compan/foreach2.java
@@ -6,6 +6,9 @@ public class foreach2 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         String name = sc.nextLine();
+        String name = sc.nextLine();
+        String name = sc.nextLine();
+        String name = sc.nextLine();
     }
 
 
