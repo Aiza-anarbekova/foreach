35af9b5d256957886ed14f636c2a9e7fddb3c12e
HP <aiza0000123@gmail.com>
2022-04-25 16:21:24 +0300

    version1

---
+++ src/com/compan/Main.java
@@ -0,0 +1,20 @@
+package com.compan;
+
+public class Main {
+
+    public static void main(String[] args) {
+        method("Amir","Jarkynai","Mahabat");
+    }
+
+     static void method (String...name){
+         for (String a:name)
+             System.out.println(a+ " I love you");
+               {
+
+         }
+
+
+
+
+    }
+}
---
+++ src/com/compan/foreach2.java
@@ -0,0 +1,12 @@
+package com.compan;
+
+import java.util.Scanner;
+
+public class foreach2 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String name = sc.nextLine();
+    }
+
+
+}
